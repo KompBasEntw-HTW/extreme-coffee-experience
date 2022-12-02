8a1e5ff5abcbeae7757f6abbf0ef5ab56b0ed99d
Vincent Dlugosch <48405050+dlugoschvincent@users.noreply.github.com>
2022-12-02 13:39:13 +0100

    feat: define coffee entity and seed db with scraped data (#1)
    * Add files via upload
    
    * Feat: Product java and Coffee java edit
    
    * docs: add some comments to run script
    
    * chore: add .DS_store to .gitignore
    
    * feat: define coffee entity and seed db
    
    * chore: move scraper into products-service
    
    * chore: add .ipynb_checkpoints to .gitignore
    
    * chore: update product service
    
    Co-authored-by: Laura Jürgensmeier <46404911+laura-jmr@users.noreply.github.com>

--- run-local-dev.sh
+++ run-local-dev.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+# Build products service 
 cd products || exit 1
 ./mvnw install
+
+# Run docker compose
 cd .. || exit 1
 docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up -d --build
+
+# If products service is specified connect dev server to docker container
 if [ "$1" = "products" ]; then
   cd products || exit 1
   ./mvnw quarkus:remote-dev
