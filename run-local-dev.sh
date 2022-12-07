bf25d91d2087c4a321e1027a31796f18161b89ee
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-12-07 20:39:29 +0100

    feat: change product service to gradle

--- run-local-dev.sh
+++ run-local-dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Build products service 
 cd products || exit 1
-./mvnw install
+./gradlew build
 
 # Run docker compose
 cd .. || exit 1
@@ -10,5 +10,5 @@ docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up -d --build
 # If products service is specified connect dev server to docker container
 if [ "$1" = "products" ]; then
   cd products || exit 1
-  ./mvnw quarkus:remote-dev
+  ./gradlew quarkusRemoteDev
 fi
