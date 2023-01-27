1bc270caf4f6f14c1cb11c725ac54d4233b80572
Vincent Dlugosch <dlugoschvincent@gmail.com>
2023-01-27 16:08:09 +0100

    chore: add checkout service scaffolding

--- run-local-dev.sh
+++ run-local-dev.sh
@@ -9,6 +9,11 @@ cd basket || exit 1
 ./gradlew build
 cd .. || exit 1
 
+# Build checkout service 
+cd checkout || exit 1
+./gradlew build
+cd .. || exit 1
+
 # Run docker compose
 docker compose --env-file ./.env.local -f docker-compose.yaml -f docker-compose.dev.yaml up -d --build
 
