4f0a990a6ee7259d8624285f41a564a8505ff627
Vincent Dlugosch <dlugoschvincent@gmail.com>
2023-02-17 14:01:10 +0300

    feat: add checkout service to run-local-dev

--- run-prod.sh
+++ run-prod.sh
@@ -9,6 +9,11 @@ cd basket || exit 1
 ./gradlew build
 cd .. || exit 1
 
+# Build checkout service 
+cd checkout || exit 1
+./gradlew build
+cd .. || exit 1
+
 # Run docker compose
 docker compose --env-file ./.env -f docker-compose.yaml up -d --build
 
